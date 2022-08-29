d92bff5784d85fb9f444e94d5a22ce20e2adcfb7
lmichratsimbazafy <ratsimbazafylmich@gmail.com>
2022-08-29 18:19:47 +0200

    fix date formatter

--- server/src/helpers/dateFormatter.ts
+++ server/src/helpers/dateFormatter.ts
@@ -1,6 +1,6 @@
 export class DateFormatterHelpers {
   static dateToString = (date: Date): string => {
-    return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
   };
 
   static stringToDate = (stringDate: string): Date => {
