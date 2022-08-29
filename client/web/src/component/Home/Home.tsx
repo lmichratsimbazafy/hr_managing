f45d86480f34f2fc4f38a25201a1e20845aed1d5
lmichratsimbazafy <ratsimbazafylmich@gmail.com>
2022-08-30 01:59:48 +0200

    handle route better

--- client/web/src/component/Home/Home.tsx
+++ client/web/src/component/Home/Home.tsx
@@ -1,11 +1,33 @@
-import ConsultantsList from "../consultantsList/ConsultantsList";
+import { Box } from "@mui/material";
+import React, { Suspense } from "react";
+import { Outlet, Route, Routes } from "react-router-dom";
+// import ConsultantsList from "../consultantsList/ConsultantsList";
+import Loader from "../loader/Loader";
 import StatusList from "../statusList/StatusList";
+
+const ConsultantDetails = React.lazy(
+  () => import("../consultantDetails/ConsultantDetails")
+);
+const ConsultantsList = React.lazy(
+  () => import("../consultantsList/ConsultantsList")
+);
 
 const Home = () => {
   return (
     <div>
-      <StatusList />
-      <ConsultantsList />
+      {/* <ConsultantsList /> */}
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <StatusList />
+        <Suspense {...{ fallback: <Loader /> }}>
+          <Routes>
+            <Route {...{ path: "/consultants", element: <ConsultantsList /> }}>
+              <Route {...{ path: ":id", element: <ConsultantDetails /> }} />
+            </Route>
+          </Routes>
+        </Suspense>
+      </Box>
+      <Outlet />
     </div>
   );
 };
